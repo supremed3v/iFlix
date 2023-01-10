b1a4784b841cda93a4d1dd5e0e685a693cfed6f1
supremed3v <saadsiddiqui-programmer@protonmail.com>
2023-01-10 19:11:35 +0500

    server setup with DB

---
+++ backend/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+export default function connectDB(){
+    const uri = process.env.MONGO_URI;
+    mongoose.set("strictQuery", false)
+    mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
+
+    const db = mongoose.connection;
+    db.on("error", console.error.bind(console, "connection error:"));
+    db.once("open", function() {
+        console.log("MoviesDB is up and running");
+    })
+}
--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,13 @@ import cookieParser from 'cookie-parser'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import fileUpload from 'express-fileupload'
+import connectDB from './db.js'
 
 dotenv.config()
 
 const app = express()
+
+connectDB()
 
 app.use(cookieParser())
 app.use(cors())
@@ -24,8 +27,9 @@ process.on('uncaughtException', (err) => {
     console.log('uncaughtException', err.stack)
     process.exit(1)
 })
+const port = process.env.PORT
 
-const server = app.listen(3000, () => {
+const server = app.listen(port, () => {
     console.log('Server is running on port 3000')
 })
 
